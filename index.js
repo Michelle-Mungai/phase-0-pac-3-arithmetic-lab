77724c5c87f59d739f25d1d1beff7216afb0df3f
Michelle-Mungai <chellymungai@gmail.com>
2022-11-21 09:26:24 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a , b){
+    return a + b;
+}
+function subtract(b , a ){
+    return b - a;
+}
+function multiply(x , y ){
+    return x * y;
+}
+function divide(x , y ){
+     return x / y;
+}
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n){
+    parseInt(n);
+    return n;
+}
+function makeInt(n){
+    return parseInt(n, 10);    
+}
+function preserveDecimal(n){
+    return parseFloat(n);    
+}
